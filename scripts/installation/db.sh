0d4336f19decd148aaadf8b91002afba93ea1f23
Aditya Mahale <aditya.mahale@enterprisedb.com>
2021-07-01 23:37:46 +0530

    Include PG option

--- scripts/installation/db.sh
+++ scripts/installation/db.sh
@@ -2,33 +2,57 @@
 # Script to install Postgres or EPAS database
 
 # Global variables
-DATABASE_TYPE=$1
-DATABASE_VERSION=$2
-OS_TYPE=$3
-USERNAME=$4
-PASSWORD=$5
+OS_TYPE=$1
+USERNAME=$2
+PASSWORD=$3
+EPAS_REPO_URL=https://yum.enterprisedb.com/edbrepos/edb-repo-latest.noarch.rpm
 
 # Function definitions - start
 
-# Installs database server on CentOS7/RHEL7 platform
+# Installs EPAS database server on CentOS7/RHEL7 platform
 installEPASC7 () {
-  yum -y install https://yum.enterprisedb.com/edbrepos/edb-repo-latest.noarch.rpm
+  yum -y install $EPAS_REPO_URL
   sed -i "s@<username>:<password>@$USERNAME:$PASSWORD@" /etc/yum.repos.d/edb.repo
   yum -y install "edb-as$DATABASE_VERSION-server"
   yum -y install sudo
 }
 
-# Installs database server on CentOS8/RHEL8 platform
+# Installs EPAS database server on CentOS8/RHEL8 platform
 installEPASC8 () {
-  dnf -y install https://yum.enterprisedb.com/edbrepos/edb-repo-latest.noarch.rpm
+  dnf -y install $EPAS_REPO_URL
   sed -i "s@<username>:<password>@$USERNAME:$PASSWORD@" /etc/yum.repos.d/edb.repo
   dnf -qy module disable postgresql
   dnf -y install "edb-as$DATABASE_VERSION-server"
   dnf -y install sudo
 }
 
+# Installs Postgres database server on CentOS7/RHEL7 platform
+installPGC7 () {
+  yum -y install sudo
+  yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+  yum -y install "postgresql$DATABASE_VERSION-server"
+}
+
+# Installs Postgres database server on CentOS8/RHEL8 platform
+installPGC8 () {
+  dnf -y install sudo
+  dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+  dnf -qy module disable postgresql
+  dnf install -y "postgresql$DATABASE_VERSION-server"
+}
+
+# Script - start
+
 if [ "$OS_TYPE" = "centos7" ]; then
-  installEPASC7
+  if [ "$DATABASE_TYPE" = "epas" ]; then
+    installEPASC7
+  else
+    installPGC7
+  fi
 else
-  installEPASC8
+  if [ "$DATABASE_TYPE" = "epas" ]; then
+    installEPASC8
+  else
+    installPGC8
+  fi
 fi
--- scripts/setup/settings.sh
+++ scripts/setup/settings.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-DATABASE_TYPE=epas
-DATABASE_USER=enterprisedb
 PGDATA=/tmp/my_data
-PGENGINE=/usr/edb/as${DB_VERSION}/bin
-AUTH=trust
 DBPORT=5433
-DATABASE_NAME=edb
+AUTH=trust
+
+if [ "$DATABASE_TYPE" = "epas" ]; then
+  DATABASE_USER=enterprisedb
+  PGENGINE=/usr/edb/as${DATABASE_VERSION}/bin
+  DATABASE_NAME=edb
+else
+  DATABASE_USER=postgres
+  PGENGINE=/usr/pgsql-${DATABASE_VERSION}/bin
+  DATABASE_NAME=postgres
+fi
